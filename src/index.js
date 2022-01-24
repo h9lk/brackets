bd0836c69eb4b788ee3898bbaab55acc33ab3791
”Vlad <hilk.2013@yandex.ru>
2022-01-24 07:58:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = [];
+    for (let i = 0; i < str.length; i += 1) {
+        for (let j = 0; j < bracketsConfig.length; j += 1) {
+            if (str[i] === bracketsConfig[j][0]
+              && str[i] === bracketsConfig[j][1]) {
+                if (brackets.length === 0 
+                  || brackets[brackets.length - 1] !== bracketsConfig[j][0]) {
+                  brackets.push(str[i]);
+                } else {
+                  brackets.pop();
+                }
+            } else {
+                if (str[i] === bracketsConfig[j][0] 
+                  || str[i] === bracketsConfig[j][1] 
+                  && brackets[brackets.length - 1] !== bracketsConfig[j][0]) {
+                    brackets.push(str[i]);
+                } else if (str[i] === bracketsConfig[j][1] 
+                  && brackets[brackets.length - 1] === bracketsConfig[j][0]) {
+                    brackets.pop();
+                }
+            }
+        }
+
+    }
+    return brackets.length === 0;
 }
