6cba21ec1fb9e9ac8a2c11e84f3b16597fbf6d2a
”Vlad <hilk.2013@yandex.ru>
2022-01-25 07:39:32 +0300

    fix: structure

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-    let brackets = [];
-    for (let i = 0; i < str.length; i += 1) {
-        for (let j = 0; j < bracketsConfig.length; j += 1) {
-            if (str[i] === bracketsConfig[j][0]
+  const brackets = [];
+  for (let i = 0; i < str.length; i += 1) {
+    for (let j = 0; j < bracketsConfig.length; j += 1) {
+      if (str[i] === bracketsConfig[j][0]
               && str[i] === bracketsConfig[j][1]) {
-                if (brackets.length === 0 
-                  || brackets[brackets.length - 1] !== bracketsConfig[j][0]) {
-                  brackets.push(str[i]);
-                } else {
-                  brackets.pop();
-                }
-            } else {
-                if (str[i] === bracketsConfig[j][0] 
-                  || str[i] === bracketsConfig[j][1] 
-                  && brackets[brackets.length - 1] !== bracketsConfig[j][0]) {
-                    brackets.push(str[i]);
-                } else if (str[i] === bracketsConfig[j][1] 
-                  && brackets[brackets.length - 1] === bracketsConfig[j][0]) {
-                    brackets.pop();
-                }
-            }
+        if (brackets.length === 0
+            || brackets[brackets.length - 1] !== bracketsConfig[j][0]) {
+          brackets.push(str[i]);
+        } else {
+          brackets.pop();
         }
-
+      } else if (str[i] === bracketsConfig[j][0]
+                  || (str[i] === bracketsConfig[j][1]
+                  && brackets[brackets.length - 1] !== bracketsConfig[j][0])) {
+        brackets.push(str[i]);
+      } else if (str[i] === bracketsConfig[j][1]
+                  && brackets[brackets.length - 1] === bracketsConfig[j][0]) {
+        brackets.pop();
+      }
     }
-    return brackets.length === 0;
+  }
+  return brackets.length === 0;
 }
+
